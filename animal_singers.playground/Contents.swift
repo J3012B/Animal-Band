bf8984ad35209c045a2f7dde1f58172bc9205df6
Josef Büttgen <josefbuettgen@gmail.com>
2018-03-14 18:57:58 +0100

    initial commit

---
+++ animal_singers.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: A SpriteKit based Playground
+
+import PlaygroundSupport
+import SpriteKit
+
+class GameScene: SKScene {
+    
+
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    
+    /*
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
+    }
+ 
+ */
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+if let scene = GameScene(fileNamed: "GameScene") {
+    // Set the scale mode to scale to fit the window
+    scene.scaleMode = .aspectFill
+    
+    // Present the scene
+    sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
