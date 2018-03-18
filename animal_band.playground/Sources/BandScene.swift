78982a830423699de8dcc6373eb20c6d8cc7f288
Josef Büttgen <josefbuettgen@gmail.com>
2018-03-18 14:34:58 +0100

    * added stage sprite * added play button (but not positioned right yet)

--- animal_band.playground/Sources/BandScene.swift
+++ animal_band.playground/Sources/BandScene.swift
@@ -5,6 +5,8 @@ public class BandScene: SKScene {
     
     var containerSize: CGSize!
     
+    var stage: SKSpriteNode!
+    
     public override init(size: CGSize) {
         super.init(size: size)
         
@@ -20,6 +22,14 @@ public class BandScene: SKScene {
         addElements()
     }
     
+    /*
+     ===================================================================
+     ============================== BAND ===============================
+     */
+    
+    private func playSong() {
+        print("Band begins to play a song")
+    }
     
     /*
      ===================================================================
@@ -55,13 +65,20 @@ public class BandScene: SKScene {
      */
     
     private func addElements() {
-        addBackground()
-        addGround()
+        addSetting()
         addAnimals()
+        addMenu()
+    }
+    
+    private func addSetting() {
+        addBackground()
+        addWall()
+        addStage()
+        addGround()
     }
     
     private func addBackground() {
-        let background = SKSpriteNode(color: UIColor(hex: "A3EEFF"), size: CGSize(width: frame.width, height: frame.height))
+        let background = SKSpriteNode(color: UIColor.gray, size: CGSize(width: frame.width, height: frame.height))
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0.0, y: 0.0)
         background.zPosition = -100
@@ -70,6 +87,40 @@ public class BandScene: SKScene {
         self.addChild(background)
     }
     
+    private func addWall() {
+        let wall = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: frame.height * 7 / 16))
+        wall.position = CGPoint(x: 0.0, y: frame.height)
+        wall.anchorPoint = CGPoint(x: 0.0, y: 1.0)
+        wall.zPosition = -91
+        
+        self.addChild(wall)
+    }
+    
+    private func addStage() {
+        let stageTex = SKTexture(imageNamed: "stage.png")
+        stage = SKSpriteNode(color: UIColor.brown, size: CGSize(width: frame.width, height: frame.height * 5 / 16))
+        stage.position = CGPoint(x: 0.0, y: frame.height * 0.25)
+        stage.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        stage.zPosition = -90
+        stage.texture = stageTex
+        stage.texture?.filteringMode = .nearest
+        
+        self.addChild(stage)
+    }
+    
+    private func addGround() {
+        let groundTex = SKTexture(imageNamed: "ground.png")
+        let ground = SKSpriteNode(color: UIColor.blue, size: CGSize(width: frame.width, height: frame.height * 0.25))
+        ground.position = CGPoint(x: 0, y: 0)
+        ground.anchorPoint = CGPoint(x: 0, y: 0)
+        ground.zPosition = -89
+        ground.texture = groundTex
+        ground.texture?.filteringMode = .nearest
+        
+        self.addChild(ground)
+    }
+    
+    /*
     private func addGround() {
         let groundTex = SKTexture(imageNamed: "ground.png")
         let ground = SKSpriteNode(texture: groundTex, size: CGSize(width: 2*frame.width, height: 0.4 * containerSize.height))
@@ -82,16 +133,38 @@ public class BandScene: SKScene {
         ground.texture?.filteringMode = .nearest
         
         self.addChild(ground)
-    }
+    }*/
+ 
     
     private func addAnimals() {
-        let animalPianoCat = SKSpriteNode(imageNamed: "piano_cat.png")
+        let animalPianoCat = SKSpriteNode(imageNamed: "Animals/Piano_Cat/piano_cat.png")
         animalPianoCat.position = CGPoint(x: 200.0, y: 100.0)
-        animalPianoCat.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        animalPianoCat.anchorPoint = CGPoint(x: 0.0, y: 0.0)
         animalPianoCat.texture?.filteringMode = .nearest;
         animalPianoCat.scaleTo(newHeight: 0.1875 * frame.height)
         
-        self.addChild(animalPianoCat)
+        stage.addChild(animalPianoCat)
+    }
+    
+    
+    private func addMenu() {
+        let menuWidth = frame.width * 0.9
+        let menuHeight = frame.height * 0.1
+        let menuMargin = (frame.width - menuWidth) * 0.5
+        let menu = SKSpriteNode(color: UIColor.green, size: CGSize(width: menuWidth, height: menuHeight))
+        menu.position = CGPoint(x: menuMargin, y: frame.height - menuMargin)
+        menu.anchorPoint = CGPoint(x: 0.0, y: 1.0)
+        menu.zPosition = 100
+        
+        /* Play Button */
+        let playBtn = SKButton(defaultButtonImage: "Menu/Play_Button/playButton_default.png", activeButtonImage: "Menu/Play_Button/playButton_active.png", buttonAction: playSong)
+        playBtn.scaleTo(newHeight: menu.frame.height)
+        playBtn.position = CGPoint(x: menu.frame.width, y: 0.0)
+        playBtn.anchorPoint = CGPoint(x: 0.0, y: 0.5)
+        
+        menu.addChild(playBtn)
+        
+        self.addChild(menu)
     }
 }
 
---
+++ animal_band.playground/Sources/SKButton.swift
@@ -0,0 +1,57 @@
+import Foundation
+import SpriteKit
+
+public class SKButton: SKSpriteNode {
+    private var defaultButton: SKSpriteNode
+    private var activeButton: SKSpriteNode
+    private var action: () -> Void
+    
+    public init(defaultButtonImage: String, activeButtonImage: String, buttonAction: @escaping () -> Void) {
+        defaultButton = SKSpriteNode(imageNamed: defaultButtonImage)
+        activeButton = SKSpriteNode(imageNamed: activeButtonImage)
+        defaultButton.texture?.filteringMode = .nearest
+        activeButton.texture?.filteringMode = .nearest
+        
+        activeButton.isHidden = true
+        action = buttonAction
+        
+        super.init(texture: nil, color: UIColor.clear, size: defaultButton.size)
+        
+        isUserInteractionEnabled = true
+        addChild(defaultButton)
+        addChild(activeButton)
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeButton.isHidden = false
+        defaultButton.isHidden = true
+    }
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = Array(touches)[0] as UITouch
+        let location: CGPoint = touch.location(in: self)
+        
+        if defaultButton.contains(location) {
+            activeButton.isHidden = false
+            defaultButton.isHidden = true
+        } else {
+            activeButton.isHidden = true
+            defaultButton.isHidden = false
+        }
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = Array(touches)[0] as UITouch
+        let location: CGPoint = touch.location(in: self)
+        
+        if defaultButton.contains(location) {
+            action()
+        }
+        
+        activeButton.isHidden = true
+        defaultButton.isHidden = false
+    }
+}
